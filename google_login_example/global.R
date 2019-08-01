e22cc059b9bb66bdd8f18df97fd3d437a0884ece
Kota <kotamine@gmail.com>
2019-08-01 13:52:59 -0500

    misc

--- google_login_example/global.R
+++ google_login_example/global.R
@@ -11,8 +11,9 @@ library(googleID)
 CLIENT_ID      <- "256934643886-8pnre1d6e4o7rgm941cnb9ehq43dlk63.apps.googleusercontent.com"
 CLIENT_SECRET  <- "Kk1v2MPAI-6DXF22v60B4o1t"
 
-CLIENT_URL     <-  "https://kotamine.shinyapps.io/google_login_example/"
+# CLIENT_URL     <-  "https://kotamine.shinyapps.io/google_login_example/"
 # CLIENT_URL     <-  'http://127.0.0.1:4488'  # URL on my desktop
+CLIENT_URL     <-  'http://localhost:4488'   # URL on my desktop
 # Run this as: shiny::runApp(port=4488) on desktop 
 
 options("googleAuthR.webapp.client_id" = CLIENT_ID)
